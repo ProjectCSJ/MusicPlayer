a7b2495250b2da64c60103e976ceb43c2dbaf3cd
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-27 20:56:57 +0900

    Build an empty bot

--- Program.cs
+++ Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient? _client;
+
+    public async Task MainAsync()
+    {
+        var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+        _client = new DiscordSocketClient(_config);
+
+        _client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = ConfigurationManager.AppSettings.Get("TOKEN");
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        _client.MessageUpdated += MessageUpdated;
+        _client.Ready += () =>
+        {
+            Console.WriteLine("Bot is connected!");
+            return Task.CompletedTask;
+        };
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+    private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+    {
+        // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+        var message = await before.GetOrDownloadAsync();
+        Console.WriteLine($"{message} -> {after}");
+    }
+}
+
+public class LoggingService
+{
+    public LoggingService(DiscordSocketClient client, CommandService command)
+    {
+        client.Log += LogAsync;
+        command.Log += LogAsync;
+    }
+    private Task LogAsync(LogMessage message)
+    {
+        if (message.Exception is CommandException cmdException)
+        {
+            Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+                + $" failed to execute in {cmdException.Context.Channel}.");
+            Console.WriteLine(cmdException);
+        }
+        else
+            Console.WriteLine($"[General/{message.Severity}] {message}");
+
+        return Task.CompletedTask;
+    }
+}
