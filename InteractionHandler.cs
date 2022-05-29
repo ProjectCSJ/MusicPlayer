7d8107e5b4185c8202a1f88c5f34e6bf11a6115b
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-29 14:28:44 +0900

    Setup interaction handler

---
+++ InteractionHandler.cs
@@ -0,0 +1,36 @@
+namespace MusicPlayer
+{
+    public class InteractionHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _commands;
+        private readonly IServiceProvider _services;
+
+        public InteractionHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            _client = client;
+            _commands = commands;
+            _services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            _client.InteractionCreated += HandleInteraction;
+        }
+
+        private async Task HandleInteraction(SocketInteraction arg)
+        {
+            try
+            {
+                var ctx = new SocketInteractionContext(_client, arg);
+                await _commands.ExecuteCommandAsync(ctx, _services);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,69 +1,47 @@
 ﻿public class Program
 {
-    static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-
-    private DiscordSocketClient? _client;
-
+    public static Task Main() => new Program().MainAsync();
     public async Task MainAsync()
     {
-        var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
-        _client = new DiscordSocketClient(_config);
 
-        _client.Log += Log;
 
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        using IHost host = Host.CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+         services
+         .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+         {
+             GatewayIntents = GatewayIntents.AllUnprivileged,
+             AlwaysDownloadUsers = true
+         }
+         ))
+         .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+         .AddSingleton<MusicPlayer.InteractionHandler>()
+         .AddSingleton(x => new CommandService())
+         )
+         .Build();
+        await RunAsync(host);
+    }
+    public async Task RunAsync(IHost host)
+    {
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        IServiceProvider provider = serviceScope.ServiceProvider;
+
+        var _client = provider.GetRequiredService<DiscordSocketClient>();
+        var slashCommands = provider.GetRequiredService<InteractionService>();
+        await provider.GetRequiredService<MusicPlayer.InteractionHandler>().InitializeAsync();
+
+        _client.Log += async (LogMessage messages) => Console.WriteLine(messages.Message);
+        slashCommands.Log += async (LogMessage messages) => Console.WriteLine(messages.Message);
+
+        _client.Ready += async () =>
+        {
+            Console.WriteLine($"Login as{_client.CurrentUser.Username.ToString()}");
+            await slashCommands.RegisterCommandsGloballyAsync(true);
+        };
         var token = ConfigurationManager.AppSettings.Get("TOKEN");
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
-        _client.MessageUpdated += MessageUpdated;
-        _client.Ready += () =>
-        {
-            Console.WriteLine("Bot is connected!");
-            return Task.CompletedTask;
-        };
-
-        // Block this task until the program is closed.
         await Task.Delay(-1);
-    }
-    private Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
-    private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
-    {
-        // If the message was not in the cache, downloading it will result in getting a copy of `after`.
-        var message = await before.GetOrDownloadAsync();
-        Console.WriteLine($"{message} -> {after}");
-    }
-}
-
-public class LoggingService
-{
-    public LoggingService(DiscordSocketClient client, CommandService command)
-    {
-        client.Log += LogAsync;
-        command.Log += LogAsync;
-    }
-    private Task LogAsync(LogMessage message)
-    {
-        if (message.Exception is CommandException cmdException)
-        {
-            Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
-                + $" failed to execute in {cmdException.Context.Channel}.");
-            Console.WriteLine(cmdException);
-        }
-        else
-            Console.WriteLine($"[General/{message.Severity}] {message}");
-
-        return Task.CompletedTask;
     }
 }
